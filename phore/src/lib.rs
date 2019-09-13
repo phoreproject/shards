80b634712c39a8408488858991098eae2e3e324e
Julian Meyer <julianmeyer2000@gmail.com>
2019-09-13 10:06:23 -0700

    Add nonces and remove references to libstd

--- phore/src/lib.rs
+++ phore/src/lib.rs
@@ -1,4 +1,44 @@
-use std::ffi;
+#![no_std]
+
+// Required to use the `alloc` crate and its types, the `abort` intrinsic, and a
+// custom panic handler.
+#![feature(core_intrinsics, lang_items, alloc_error_handler)]
+
+#[macro_use]
+extern crate alloc;
+
+use alloc::vec::Vec;
+
+use core::ffi;
+
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+// Need to provide a tiny `panic` implementation for `#![no_std]`.
+// This translates into an `unreachable` instruction that will
+// raise a `trap` the WebAssembly execution if we panic at runtime.
+#[panic_handler]
+#[no_mangle]
+pub fn panic(info: &::core::panic::PanicInfo) -> ! {
+    log_message(&format!("Panic: {}", info));
+
+    unsafe {
+        ::core::intrinsics::abort();
+    }
+}
+
+// Need to provide an allocation error handler which just aborts
+// the execution with trap.
+#[alloc_error_handler]
+#[no_mangle]
+pub extern "C" fn oom(_: ::core::alloc::Layout) -> ! {
+    // log_message("outy");
+
+    unsafe {
+        ::core::intrinsics::abort();
+    }
+}
 
 #[link(wasm_import_module = "phore")]
 extern {
@@ -6,13 +46,21 @@ extern {
     fn store(addr: &[u8; 32], val: &[u8; 32]);
     fn validateECDSA(hashAddr: &[u8; 32], signatureAddr: &[u8; 65], out: &mut[u8; 33]) -> i64;
     fn hash(data: *const u8, length: usize) -> [u8; 32];
-    fn loadArgument(arg_num: i32, out: *mut ffi::c_void);
+    fn loadArgument(arg_num: i32, arg_len: usize, out: *mut ffi::c_void);
     fn write_log(msg: *const u8, length: usize);
+    fn shard_number() -> i32;
 }
 
 type Hash = [u8; 32];
 type PublicKey = [u8; 33];
 type Signature = [u8; 65];
+
+pub fn get_shard_number() -> i32 {
+    let out: i32 = unsafe {
+        shard_number()
+    };
+    return out;
+}
 
 pub fn load_from_storage(addr: Hash) -> Hash {
     let out: Hash = unsafe {
@@ -45,17 +93,18 @@ pub fn hash_bytes(to_hash: &[u8]) -> [u8; 32] {
     out_hash
 }
 
-pub fn load_argument(arg_num: i32, arg_size: usize) -> Box<[u8]> {
-    let out = vec![0; arg_size];
+pub fn load_argument(arg_num: i32, arg_size: usize) -> Vec<u8> {
+    let mut out = Vec::with_capacity(arg_size);
+    out.resize(arg_size, 0);
 
     unsafe {
-        loadArgument(arg_num, out.as_ptr() as *mut ffi::c_void);
+        loadArgument(arg_num, arg_size, out.as_ptr() as *mut ffi::c_void);
     }
 
-    return out.into_boxed_slice();
+    out
 }
 
-pub fn log_message(msg: String) {
+pub fn log_message(msg: &str) {
     unsafe {
         write_log(msg.as_ptr(), msg.len());
     }
@@ -69,4 +118,23 @@ pub fn num_to_hash(num: u64) -> Hash {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     ((num >> 56) & 0xFF) as u8, ((num >> 48) & 0xFF) as u8, ((num >> 40) & 0xFF) as u8, ((num >> 32) & 0xFF) as u8,
     ((num >> 24) & 0xFF) as u8, ((num >> 16) & 0xFF) as u8, ((num >> 8) & 0xFF) as u8, (num & 0xFF) as u8]
+}
+
+pub fn concat_bytes<'a>(args: &'a [&[u8]]) -> Vec<u8> {
+    let total_length = args.iter().fold(0usize, |sum, val| sum + val.len());
+
+    let mut out = Vec::with_capacity(total_length);
+    for arg in args.iter() {
+        out.extend_from_slice(arg);
+    }
+
+    out
+}
+
+pub fn get_storage_hash_path(path: &[&[u8]]) -> [u8; 32] {
+    match path {
+        [] => hash_bytes(&[0; 32]),
+        [one] => hash_bytes(one),
+        _ => hash_bytes(&concat_bytes(&[path[0], &get_storage_hash_path(&path[1 ..])]))
+    }
 }
--- transfer-shard/src/lib.rs
+++ transfer-shard/src/lib.rs
@@ -1,3 +1,9 @@
+#![no_std]
+
+extern crate wee_alloc;
+
+extern crate alloc;
+
 #[macro_use]
 extern crate lazy_static;
 
@@ -6,103 +12,135 @@ extern crate hex_literal;
 
 extern crate phore;
 
-use phore::load_argument;
-use phore::hash_bytes;
-use phore::validate_ecdsa;
-use phore::hash_to_num;
-use phore::num_to_hash;
-use phore::load_from_storage;
-use phore::save_to_storage;
+use phore::*;
 
-use std::collections::HashMap;
-
-use std::fmt::Write;
+use alloc::collections::BTreeMap;
+use alloc::vec::Vec;
 
 lazy_static!{
-    static ref PREMINE: HashMap<[u8; 32], u64> = [
-        (hex!("1fd93f10d5a9a8838b60cb0975504eeead3e65d073df53ff138308cccc4069e9"), 100000000),
+    static ref PREMINE: BTreeMap<[u8; 20], u64> = [
+        (hex!("ff6faaf7b1ca750f7d76fc2c739e34e4cb8e3599"), 100000000),
     ].iter().copied().collect();
+}
+
+fn bytes_to_u64(b: &[u8]) -> u64 {
+    if b.len() != 8 {
+        panic!("bytes_to_u64 called with improper size")
+    }
+
+    return u64::from_be_bytes([b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7]])
 }
 
 #[no_mangle]
 pub extern fn redeem_premine() -> u64 {
-    let mut to_pubkey_hash: [u8; 32] = [0; 32];
-    to_pubkey_hash.copy_from_slice(&load_argument(0, 32));
+    // load the user's address
+    let mut to_pubkey_hash: [u8; 20] = [0; 20];
+    to_pubkey_hash.copy_from_slice(&load_argument(0, 20));
 
+    // ensure the user has a premined balance
     if !PREMINE.contains_key(&to_pubkey_hash) {
         return 1;
     }
 
-    let mut redemption_message = String::from("redeem ");
+    // get the status of the redemption at "redeem/{ADDRESS}"
+    let redemption_path = get_storage_hash_path(&[b"redeem", &to_pubkey_hash]);
+    let redemption_status = hash_to_num(load_from_storage(redemption_path));
 
-    for &byte in to_pubkey_hash.iter() {
-        redemption_message.push_str(&format!("{:02x}", byte));
-    }
-
-    let message_hash = hash_bytes(redemption_message.as_bytes());
-
-    let redemption_status = hash_to_num(load_from_storage(message_hash));
-
+    // ensure it isn't redeemed
     if redemption_status != 0 {
         return 2;
     }
 
-    let old_balance = hash_to_num(load_from_storage(to_pubkey_hash));
+    // get the user's balance
+    let to_balance_path = get_storage_hash_path(&[b"balance", &to_pubkey_hash]);
+    let old_balance = hash_to_num(load_from_storage(to_balance_path));
 
+    // add to the user's balance
     let redemption_amount = PREMINE.get(&to_pubkey_hash).unwrap();
+    save_to_storage(to_balance_path, num_to_hash(old_balance + redemption_amount));
 
-    save_to_storage(to_pubkey_hash, num_to_hash(old_balance + redemption_amount));
-
-    save_to_storage(message_hash, num_to_hash(1));
+    // mark the premine as redeemed
+    save_to_storage(redemption_path, num_to_hash(1));
 
     return 0;
 }
 
 #[no_mangle]
 pub extern fn transfer_to_address() -> u64 {
-    let mut from_pubkey: [u8; 33] = [0; 33];
-    from_pubkey.copy_from_slice(&load_argument(0, 33));
+    // transaction data including: version, amount, nonce, to_pubkey_hash
+    let mut tx_data: [u8; 33] = [0; 33];
+    let test = &load_argument(0, 33);
+    tx_data.copy_from_slice(test);
+
+    // signature of hash of tx_data
     let mut signature: [u8; 65] = [0; 65];
     signature.copy_from_slice(&load_argument(1, 65));
-    let mut to_pubkey_hash: [u8; 32] = [0; 32];
-    to_pubkey_hash.copy_from_slice(&load_argument(2, 32));
-    let mut amount_bytes: [u8; 8] = [0; 8];
-    amount_bytes.copy_from_slice(&load_argument(3, 8));
 
-    let amount = u64::from_be_bytes(amount_bytes);
+    // from_pubkey_hash: sender's address
+    let mut from_pubkey_hash: [u8; 20] = [0; 20];
+    from_pubkey_hash.copy_from_slice(&load_argument(2, 20));
 
-    let from_pubkey_hash = hash_bytes(&from_pubkey);
+    // from_storage_nonces is the storage address where the nonce for the transaction is kept. If it's not 0, we
+    // know that the nonce is already used and we should reject the transaction.
+    let from_storage_nonces: [u8; 32] = get_storage_hash_path(&[b"nonce", &from_pubkey_hash, &tx_data[1 .. 5]]);
 
-    let mut message_to_sign = format!("transfer {} PHR to ", amount);
-    for &byte in to_pubkey_hash.iter() {
-        write!(&mut message_to_sign, "{:02x}", byte).ok();
-    }
-
-    let message_hash = hash_bytes(message_to_sign.as_bytes());
-
-    let (signature_output, expected_pubkey) = validate_ecdsa(message_hash, signature);
-
-    if signature_output != 0 || !expected_pubkey.iter().eq(from_pubkey.iter()) {
+    let nonce_used = hash_to_num(load_from_storage(from_storage_nonces));
+    if nonce_used != 0 {
         return 1;
     }
 
-    let old_balance = hash_to_num(load_from_storage(from_pubkey_hash));
+    // extract the hash of the pubkey we're sending to
+    let to_pubkey_hash = &tx_data[5 .. 25];
 
-    if old_balance < amount {
+    // extract the amount we're sending
+    let amount = bytes_to_u64(&tx_data[25 .. 33]);
+
+    // get the hash of the transaction data which should be the message signed
+    let message_hash = hash_bytes(&tx_data);
+
+    // recover the public key from the message and signature. This is cryptographically secure as long as
+    // an attacker can't efficiently generate a signature such that they can predict the public key. This is
+    // the case as long as message_hash is a random oracle (like a hash)
+    let (signature_output, expected_pubkey) = validate_ecdsa(message_hash, signature);
+
+    // ensure the signature validates
+    if signature_output != 0 {
         return 2;
     }
 
-    let new_balance = old_balance - amount;
+    // get the shard number being executed
+    let shard_bytes = i32::to_be_bytes(get_shard_number());
 
-    let balance_hash = num_to_hash(new_balance);
+    // calculate the expected address given the recovered public key and the shard number
+    let mut expected_pubkey_and_shard = Vec::with_capacity(37);
+    expected_pubkey_and_shard.extend_from_slice(&shard_bytes);
+    expected_pubkey_and_shard.extend_from_slice(&expected_pubkey);
+    let expected_pubkey_hash = hash_bytes(expected_pubkey_and_shard.as_slice());
 
-    save_to_storage(from_pubkey_hash, balance_hash);
+    // verify the expected address matches the given address
+    if !expected_pubkey_hash[0 .. 20].iter().eq(from_pubkey_hash.iter()) {
+        return 3;
+    }
 
-    let to_balance = hash_to_num(load_from_storage(to_pubkey_hash));
+    // get the storage addresses of the sender and recipient
+    let from_storage: [u8; 32] = get_storage_hash_path(&[b"balance", &from_pubkey_hash]);
+    let to_storage: [u8; 32] = get_storage_hash_path(&[b"balance", &to_pubkey_hash]);
 
-    let to_balance_hash = num_to_hash(to_balance + amount);
+    // ensure the user has sufficient balance
+    let old_balance = hash_to_num(load_from_storage(from_storage));
+    if old_balance < amount {
+        return 4;
+    }
 
-    save_to_storage(to_pubkey_hash, to_balance_hash);
+    // update the sender's balance
+    save_to_storage(from_storage, num_to_hash(old_balance - amount));
+
+    // mark the nonce as used
+    save_to_storage(from_storage_nonces, num_to_hash(1));
+
+    // update the recipient's balance
+    let to_balance = hash_to_num(load_from_storage(to_storage));
+    save_to_storage(to_storage, num_to_hash(to_balance + amount));
 
     return 0;
 }
